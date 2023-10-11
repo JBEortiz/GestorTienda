646d9297bd840b3182a8f8211726fc0fb4b0c1f0
JBEortiz <60776462+JBEortiz@users.noreply.github.com>
2023-10-11 11:55:12 +0200

    Update import.sql

--- gestionTienda/src/main/resources/import.sql
+++ gestionTienda/src/main/resources/import.sql
@@ -1,5 +1,4 @@
 INSERT INTO `users` (username, password, enabled, name, lastname, direction, amount_product) VALUES ('juan','$2a$10$hdTaz3vfnc2ikq7EiUyjXO1ImAN6BrEomAno0hToE7bhBAKTeJyhC',2,'juan','berenguer','juan@juan',1);
-INSERT INTO `users` (username, password, enabled, name, lastname, direction, amount_product) VALUES ('sara','$2a$10$23ZdWFN.RVCJ9MTZl52l1.sLjGo7iqsp75vjtocb5AhUmfFrpM1ZK',2,'sara','vidal','vida@vida',1);
 
 INSERT INTO `roles` (name) VALUES ('ROL_USER');
 INSERT INTO `roles` (name) VALUES ('ROL_ADMIN');
